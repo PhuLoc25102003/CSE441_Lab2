e88b1107f532ea4273bd84099ca33421cda2931c
PhuLoc25102003 <155551286+PhuLoc25102003@users.noreply.github.com>
2024-10-22 23:10:54 +0700

    add Q3IncomeTax

--- App.tsx
+++ App.tsx
@@ -1,118 +1,45 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState} from "react";
+import {View, Text, TextInput,Button} from 'react-native';
+import styles from "./style";
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+export default App = () =>{
+  const [income, setIncome] = useState('');
+  const [tax, setTax] =  useState('');
+  
+  const calculateTax = () =>{
+    const incomeAmount = parseFloat(income);
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+    if(isNaN(incomeAmount) || incomeAmount < 0){
+      setTax('Invalid income');
+      return;
+    }
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+    let taxAmount = 0;
+    if(incomeAmount <= 10000000){
+      taxAmount = incomeAmount * 0.1;
+    }else if(incomeAmount <= 50000000){
+      taxAmount = 10000000 * 0.1 + (incomeAmount - 10000000) * 0.2;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    }else{
+      taxAmount = 10000000 * 0.1 + 40000000 * 0.2 + (incomeAmount - 50000000) * 0.3;
+    }
+    setTax(`Income Tax: ${taxAmount.toFixed(2)}đ`);
+  }
+
+  return(
+    <View style = {styles.container}>
+      <Text style = {styles.title}>Income Tax Calculator</Text>
+
+      <TextInput
+      textAlign="right"
+      style = {styles.input}
+      placeholder= "Enter your income"
+      keyboardType="numeric"
+      value={income}
+      onChangeText={text => setIncome(text)}
+      />
+      <Button title="Calculate Tax" onPress={calculateTax}/>
+      <Text style = {styles.result}>{tax}</Text>
     </View>
   );
 }
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
